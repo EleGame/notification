71a4bcf0d047a9a30490ec5b427ebc6ffbabdcd6
Rafael Corsi <rafael.corsi@insper.edu.br>
2020-11-27 12:39:57 -0300

    fecha janela automatica e progress bar

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const {remote} = require('electron');
-const { shell } = require('electron')
+const {remote, shell} = require('electron');
+
+var win = remote.getCurrentWindow();
+const app = require('electron').remote.app
 
 document.getElementById('legal').onclick = function(){
-    var win = remote.getCurrentWindow();
     win.hide()
   }
 
 document.getElementById('mais').onclick = function(){
-    var win = remote.getCurrentWindow();
     win.hide()
     shell.openExternal('https://insper.github.io/Z01.1/')
   }
+
+var progress = document.getElementsByClassName('progress-bar').item(0);
+var time = 0;
+var interval = setInterval(closeWindownTimeout, 100);
+
+function closeWindownTimeout () {
+  if (time < 100) {
+    time = time + 5;
+    progress.style.width = time + "%"
+  } else {
+    clearInterval(interval);
+    setTimeout( function (){ win.hide(); }, 5000);
+  }
+};
 
